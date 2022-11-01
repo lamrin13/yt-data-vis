4181814bc40dc5fec9c91e0e3246308f33259bfe
lamrin13 <patelnirmal13595@gmail.com>
2022-11-01 11:10:08 -0400

    aligned plots

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ import {
     Inspector,
 } from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js";
 import notebook from "./temp.js";
-import define from "https://api.observablehq.com/d/2f691757ac83a209@1281.js?v=3";
+import define from "https://api.observablehq.com/d/2f691757ac83a209@1320.js?v=3";
 import bubble from "https://api.observablehq.com/d/591ec25efcb5dcd5@544.js?v=3";
+import barchart from "https://api.observablehq.com/d/72ed590acb359427.js?v=3";
 
 const main = new Runtime().module(notebook, (name) => {
     switch (name) {
@@ -28,6 +29,13 @@ const bubble_plot = new Runtime().module(bubble, (name) => {
     if(name === "chart")
         return new Inspector(
             document.querySelector("#bubble")
+        );
+})
+
+const bar_plot = new Runtime().module(barchart, (name) => {
+    if(name === "chart")
+        return new Inspector(
+            document.querySelector("#barplot")
         );
 })
 flower.redefine(
