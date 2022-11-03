1ad29f921bd327d5202d5083624b2a020b31a22a
lamrin13 <patelnirmal13595@gmail.com>
2022-11-03 00:28:50 -0400

    slides added

--- app.js
+++ app.js
@@ -52,3 +52,24 @@ export function temp(title){
         fetch(title+".json").then((resp) => resp.json())
     );
 }
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+export function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+export function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+  //   let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+  //   dots[slideIndex-1].className += " active";
+  }
+  
